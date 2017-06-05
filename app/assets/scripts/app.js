9e1f60f79fd5ac003b1910d63fb18ada4049d442
jamesbyrne89 <mail@jamestbyrne.com>
2017-06-05 22:58:48 +0100

    Added number button event listeners

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,55 @@
+const oneBtn = document.getElementById('one');
+const twoBtn = document.getElementById('two');
+const threeBtn = document.getElementById('three');
+const fourBtn = document.getElementById('four');
+const fiveBtn = document.getElementById('five');
+const sixBtn = document.getElementById('six');
+const sevenBtn = document.getElementById('seven');
+const eightBtn = document.getElementById('eight');
+const nineBtn = document.getElementById('nine');
+const input = document.getElementById('input');
+
+oneBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+twoBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+threeBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+fourBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+fiveBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+sixBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+sevenBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+eightBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
+
+nineBtn.addEventListener('click', function(){
+	input.innerText += this.innerText;
+	console.log(typeof this.innerText)
+});
