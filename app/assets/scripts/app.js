b820c9c351cd89e9b7ca971f774690026be08b43
James Byrne <mail@jamestbyrne.com>
2017-06-06 11:21:14 +0100

    Simplified event listener adding function

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,55 +1,36 @@
-const oneBtn = document.getElementById('one');
-const twoBtn = document.getElementById('two');
-const threeBtn = document.getElementById('three');
-const fourBtn = document.getElementById('four');
-const fiveBtn = document.getElementById('five');
-const sixBtn = document.getElementById('six');
-const sevenBtn = document.getElementById('seven');
-const eightBtn = document.getElementById('eight');
-const nineBtn = document.getElementById('nine');
+
 const input = document.getElementById('input');
 
-oneBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
+(function addClickEvents() {
+	const numbers = document.getElementsByClassName('button');
+	Array.prototype.forEach.call(numbers, (function(item) {
+		item.addEventListener('click', function() {
+			input.innerText += this.innerText;
+		})
+	}))
+})();
+
+
+
+const ac = document.getElementById('ac');
+
+ac.addEventListener('click', function(){
+	input.innerText = '';
 });
 
-twoBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
+const percentage = document.getElementById('percentage');
+
+percentage.addEventListener('click', function(){
+	if (input.innerText) {
+	input.innerText += '%';
+}
 });
 
-threeBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
+document.getElementById('equals').addEventListener('click', function(){
+	console.log(input.innerText);
+	console.log(typeof input.innerText);
 });
 
-fourBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
-
-fiveBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
-
-sixBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
-
-sevenBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
-
-eightBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
-
-nineBtn.addEventListener('click', function(){
-	input.innerText += this.innerText;
-	console.log(typeof this.innerText)
-});
+function joinIntegers() {
+	
+};
