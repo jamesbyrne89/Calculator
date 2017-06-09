ceb3c3ce43b34710aea289e0ba9e93fdcd23a5c7
jamesbyrne89 <mail@jamestbyrne.com>
2017-06-09 21:40:45 +0100

    Added check that input not empty for operators

---
+++ app/assets/scripts/_init.js
@@ -0,0 +1,56 @@
+function init() {
+	const numbers = document.getElementsByClassName('btn__number');
+	const equalsBtn = document.getElementById('equals');
+	const ac = document.getElementById('ac');
+	const percentage = document.getElementById('percentage');
+	const operators = document.getElementsByClassName('operator');
+
+	const _setInitialState = function _setInitialState() {
+		input.innerText = '';
+	};	
+	const _listen = function _listen() {
+		let handleInput = new InputHandler();
+		Array.prototype.forEach.call(numbers, (function(item) {
+			item.addEventListener('click', function() {
+				handleInput.getInput();
+				handleInput.updateView(this.innerText);
+			})
+		}))
+
+		equalsBtn.addEventListener('click', function() {
+			let getTotal = new InputHandler();
+			getTotal.getInput();
+			getTotal.evaluate(input.innerText);
+		}, false)
+
+		ac.addEventListener('click', function() {
+			input.innerText = '';
+			output.innerText = '';
+		});
+
+		Array.prototype.forEach.call(operators, (function(item) {
+			item.addEventListener('click', function() {
+				handleInput.getInput();
+				handleInput.checkDuplicates(this.innerText);
+				if (!handleInput.checkDuplicates(this.innerText) && input.innerText.length > 0){
+				handleInput.updateView(this.innerText);
+			}
+			else {
+				return;
+			}
+			})
+		}))
+		percentage.addEventListener('click', function(){
+			let getPercentage = new InputHandler();
+			getPercentage.getInput();
+			getPercentage.evaluate(getPercentage.handlePercent(input.innerText));
+		});
+
+	};
+
+	_setInitialState();
+	_listen();
+
+};
+
+module.exports = init;
